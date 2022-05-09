865b3309b88cffc587061a83ae3b96ca0f22987c
CarlosArango <arangotorcar@gmail.com>
2022-05-09 14:33:47 -0500

    configured prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
-import Header from "./components/organisms/Header";
-import Reports from "./pages/reports/presenter/reports";
+import './App.css';
+import Header from './components/organisms/Header';
+import Reports from './pages/reports/presenter/reports';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Reports />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Reports />
+        </div>
+    );
 }
 
 export default App;
