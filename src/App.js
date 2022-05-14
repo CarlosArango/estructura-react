77c254a4078ff0a96e91ec00b6891a1407c2cc21
CarlosArango <arangotorcar@gmail.com>
2022-05-14 11:01:47 -0500

    feat: example with mui and react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
-import './App.css';
-import Header from './components/organisms/Header';
-import Reports from './pages/reports/presenter/reports';
-
-function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Reports />
-        </div>
-    );
-}
+const App = () => {
+    return <div>dsada</div>;
+};
 
 export default App;
